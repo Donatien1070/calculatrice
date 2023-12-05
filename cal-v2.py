3336fe83645b2598c1124d766131e7d76024ad75
Donatien1070 <130686271+Donatien1070@users.noreply.github.com>
2023-12-05 11:30:12 +0100

    Update cal-v2.py

--- cal-v2.py
+++ cal-v2.py
@@ -1,13 +1,34 @@
-print("bienvenue dans la calculatrice\n")
-nb_cal_user = int(input("combien de calcule sont prévu? :"))
-for a in range(int(nb_cal_user)):
-    #le user choisie quel calcule il veut faire
-    #savoir si le user veut faire un autre calcule  
-    print("quel type de calcule vous voulez faire :")
+print("Bienvenue dans la calculatrice\n")
+nb_cal_user = int(input("Combien de calculs sont prévus? : "))
+
+for _ in range(nb_cal_user):
+    print("Quel type de calcul voulez-vous faire :")
     print("1 = addition")
     print("2 = soustraction")
     print("3 = multiplication")
     print("4 = division")
-    print("5 = racine carré")
-    user_cal_choix = int(input(" quel chiffre choisiser vous :  "))
-    
+    print("5 = racine carrée")
+
+    user_cal_choix = int(input("Quel chiffre choisissez-vous : "))
+
+    if user_cal_choix in (1, 2, 3, 4):
+        user_num1 = int(input("Quel est le premier chiffre : "))
+        user_num2 = int(input("Quel est le deuxième chiffre : "))
+
+        if user_cal_choix == 1:
+            result = user_num1 + user_num2
+        elif user_cal_choix == 2:
+            result = user_num1 - user_num2
+        elif user_cal_choix == 3:
+            result = user_num1 * user_num2
+        elif user_cal_choix == 4:
+            result = user_num1 / user_num2
+
+        print(f"Le résultat est : {result}")
+
+    elif user_cal_choix == 5:
+        user_num = int(input("Quel est le chiffre : "))
+        result = user_num ** 0.5
+        print(f"Le résultat est : {result}")
+
+    print("")
